66daad8842e277cc678e5c84c8816a9d4a5ab9cf
Noriega402 <d.noriegachajon@gmail.com>
2023-03-02 19:52:31 -0600

    plantilla 1

---
+++ src/main/java/com/programacionuno/challengealura/ChallengeAlura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.programacionuno.challengealura;
+
+/**
+ *
+ * @author Daniel Noriega
+ */
+public class ChallengeAlura {
+
+    public static void main(String[] args) {
+        
+    }
+}

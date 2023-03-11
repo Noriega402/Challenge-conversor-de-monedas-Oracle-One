1e92f2fc49ff215816f277bfffd7537a964cd5b3
Noriega402 <d.noriegachajon@gmail.com>
2023-03-11 01:45:16 -0600

    Cambiar iconos a un solo archivo

---
+++ src/main/java/com/programacionuno/challengealura/Iconos.java
@@ -0,0 +1,16 @@
+package com.programacionuno.challengealura;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Daniel Noriega
+ */
+public class Iconos {
+    ImageIcon coin = new ImageIcon("src/images/coinsTwo.png"); //agregar imagen
+    ImageIcon icon = new ImageIcon("src/images/coinsOne.png"); //agregar imagen
+    ImageIcon error = new ImageIcon("src/images/error.png"); //agregar imagen
+    ImageIcon temp = new ImageIcon("src/images/temperatura.png"); //agregar imagen
+    ImageIcon medida = new ImageIcon("src/images/medida.png"); //agregar imagen
+    ImageIcon check = new ImageIcon("src/images/check.png"); //agregar imagen
+}

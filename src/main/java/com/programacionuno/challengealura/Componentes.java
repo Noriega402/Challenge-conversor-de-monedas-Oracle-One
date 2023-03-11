6cac1762d6912e6bd1d1ff3470aa225c92e4e79c
Noriega402 <d.noriegachajon@gmail.com>
2023-03-10 23:44:21 -0600

    Agregar mensajes de finalizar programa y a prueba de errores para ingreso de datos

--- src/main/java/com/programacionuno/challengealura/Componentes.java
+++ src/main/java/com/programacionuno/challengealura/Componentes.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.programacionuno.challengealura;
 
 import java.awt.Image;
@@ -24,6 +20,15 @@ public class Componentes {
         "Quetzal a Won Coreano", "Quetzal a Pesos Mexicanos", "Dolar a Quetzal", "Euro a Quetzal",
         "Libra a Quetzal", "Yen a Quetzal", "Won Coreano a Quetzal", "Pesos Mexicanos a Quetzal"};
 
+    public void terminarPrograma() {
+        int terminar = JOptionPane.showConfirmDialog(null, "¿Deseas finalizar el programa?", "Finalizar", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (terminar == 0){
+            JOptionPane.showMessageDialog(null, "Programa finalizado", "Finalizado", 0, check);
+            System.exit(0);
+        }
+        else menu();
+    }
+
     public void menu() {
         String titulo = "CONVERSOR ALURA CHALLENGE";
         String mensaje = "Selecciona una opcion";
@@ -32,31 +37,70 @@ public class Componentes {
         String n = (String) JOptionPane.showInputDialog(null, mensaje,
                 titulo, JOptionPane.QUESTION_MESSAGE, icon, opciones, opciones[0]);
 
-        switch (n) {
-            case "Convertir Moneda":
-                JOptionPane.showMessageDialog(null, "Elegiste convertir moneda", titulo, 1, coin);
-                menuMonedas();
-                break;
-            case "Convertir temperatura":
-                JOptionPane.showMessageDialog(null, "Elegiste convertir temperatura", titulo, 0, temp);
-                break;
-            case "Convertir medidas de longitud":
-                JOptionPane.showMessageDialog(null, "Elegiste convertir medidas", titulo, JOptionPane.QUESTION_MESSAGE, medida);
-                break;
-            default:
-                throw new AssertionError("Error al seleccionar una opcion");
+        if (n == null) {
+            terminarPrograma();
+        } else {
+            switch (n) {
+                case "Convertir Moneda":
+                    JOptionPane.showMessageDialog(null, "Elegiste convertir moneda", titulo, 1, coin);
+                    menuMonedas();
+                    break;
+                case "Convertir temperatura":
+                    JOptionPane.showMessageDialog(null, "Elegiste convertir temperatura", titulo, 0, temp);
+                    break;
+                case "Convertir medidas de longitud":
+                    JOptionPane.showMessageDialog(null, "Elegiste convertir medidas", titulo, JOptionPane.QUESTION_MESSAGE, medida);
+                    break;
+                default:
+                    throw new AssertionError("Error al seleccionar una opcion");
+            }
         }
     }
 
-    public void mostarConversion(double valor, int titulo, String tMoneda, boolean opcion) {
+    public void mostarConversion(float valor, int titulo, String tMoneda, boolean opcion) {
+        boolean validar = true;
         Moneda cambio = new Moneda();
-        double resultado, dinero;
+        double resultado;
+        float dinero;
         cambio.setValor(valor);
         cambio.setIsDivide(opcion);
-        dinero = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el monto", conversiones[titulo], 1));
-        cambio.setDinero(dinero);
-        resultado = cambio.getConversor();
-        JOptionPane.showMessageDialog(null, "Tienes " + resultado + tMoneda, conversiones[titulo], 0, icon);
+
+        do {
+            try {
+                dinero = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite el monto", conversiones[titulo], 1));
+                cambio.setDinero(dinero);
+                resultado = cambio.getConversor();
+                JOptionPane.showMessageDialog(null, "Tienes " + resultado + tMoneda, conversiones[titulo], 0, icon);
+                validar = false;
+                terminarPrograma();
+            }catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "El valor debe ser un numero...", "Error", 0, error);
+            }catch(NullPointerException e){
+                int regresar = JOptionPane.showConfirmDialog(null, "¿Deseas regresar al submenu?", "Regresar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+                if(regresar == 0){
+                    validar = false;
+                    menuMonedas();
+                }
+            }
+        } while (validar);
+        
+        terminarPrograma();
+    }
+
+    public void regresarMenuPrincipal() {
+        int opcion = JOptionPane.showConfirmDialog(null, "¿Quieres regresar al menu principal?", "Regresar", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, icon);
+        if (opcion == 0) menu();
+        else menuMonedas();
+    }
+    
+    public void continuarPrograma(){
+        int opcion = JOptionPane.showConfirmDialog(null, "¿Desea continuar con el programa?", "Continuar Programa", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if(opcion == 0) menu();
+        else{
+            JOptionPane.showMessageDialog(null, "Programa terminado", "Terminado", 0, check);
+            System.exit(0);
+        }
     }
 
     public void menuMonedas() {
@@ -65,48 +109,49 @@ public class Componentes {
 
         String opciones = (String) JOptionPane.showInputDialog(null, mensaje, titulo,
                 JOptionPane.QUESTION_MESSAGE, coin, conversiones, conversiones[0]);
-        System.out.println(opciones);
-
-        switch (opciones) {
-            case "Quetzal a Dolar":
-                mostarConversion(0.1280, 0, " dolares", true);
-                break;
-            case "Quetzal a Euro":
-                mostarConversion(0.1204, 1, " euros", false);
-                break;
-            case "Quetzal a Libra":
-                mostarConversion(0.1063, 2, " libras", false);
-                break;
-            case "Quetzal a Yen":
-                mostarConversion(17.3798, 3, " yenes", false);
-                break;
-            case "Quetzal a Won Coreano":
-                mostarConversion(165.97, 4, " wones", false);
-                break;
-            case "Quetzal a Pesos Mexicanos":
-                mostarConversion(2.30, 5, " pesos mexicanos", false);
-                break;
-            case "Dolar a Quetzal":
-                mostarConversion(7.8147, 6, " quetzales", false);
-                break;
-            case "Euro a Quetzal":
-                mostarConversion(8.3191, 7, " quetzales", false);
-                break;
-            case "Libra a Quetzal":
-                mostarConversion(9.4060, 8, " quetzales", false);
-                break;
-            case "Yen a Quetzal":
-                mostarConversion(0.05753, 9, " quetzales", false);
-                break;
-            case "Won Coreano a Quetzal":
-                mostarConversion(0.006025,10, " quetzales", false);
-                break;
-            case "Pesos mexicanos a Quetzal":
-                mostarConversion(0.4348,11, " quetzales", false);
-                break;
-            default:
-                JOptionPane.showMessageDialog(null, "Ocurrio un error", "Error", 2, error);
-                throw new AssertionError("Error de opcion");
+        if (opciones == null) {
+            regresarMenuPrincipal();
+        } else {
+            switch (opciones) {
+                case "Quetzal a Dolar":
+                    mostarConversion(0.1280f, 0, " dolares", true);
+                    break;
+                case "Quetzal a Euro":
+                    mostarConversion(0.1204f, 1, " euros", false);
+                    break;
+                case "Quetzal a Libra":
+                    mostarConversion(0.1063f, 2, " libras", false);
+                    break;
+                case "Quetzal a Yen":
+                    mostarConversion(17.3798f, 3, " yenes", false);
+                    break;
+                case "Quetzal a Won Coreano":
+                    mostarConversion(165.97f, 4, " wones", false);
+                    break;
+                case "Quetzal a Pesos Mexicanos":
+                    mostarConversion(2.30f, 5, " pesos mexicanos", false);
+                    break;
+                case "Dolar a Quetzal":
+                    mostarConversion(7.8147f, 6, " quetzales", false);
+                    break;
+                case "Euro a Quetzal":
+                    mostarConversion(8.3191f, 7, " quetzales", false);
+                    break;
+                case "Libra a Quetzal":
+                    mostarConversion(9.4060f, 8, " quetzales", false);
+                    break;
+                case "Yen a Quetzal":
+                    mostarConversion(0.05753f, 9, " quetzales", false);
+                    break;
+                case "Won Coreano a Quetzal":
+                    mostarConversion(0.006025f, 10, " quetzales", false);
+                    break;
+                case "Pesos mexicanos a Quetzal":
+                    mostarConversion(0.4348f, 11, " quetzales", false);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Ocurrio un error", "Error", 2, error);
+            }
         }
-    }
+    }          
 }
